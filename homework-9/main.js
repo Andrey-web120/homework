6f508fa7a8f8911ddb386c342fdc5e3c7eea9c8f
Andrey-web120 <perepelicaa44gmail.co@mail.ru>
2026-01-12 22:18:29 +0300

    Add files via upload

---
+++ homework-9/main.js
@@ -0,0 +1,15 @@
+const firstName = "Andrey";
+const lastName = "Pepel";
+const isStudent = true;
+const age = 16
+const currentYear = 2026
+const birthYear = currentYear - age
+console.log("Меня зовут" ,firstName ,lastName + "," ,"мне" , age +"." ,"Я ученик курса:" ,isStudent  )
+// let a = '123';
+// let b = +'456';
+// let c = Number('789');
+// let d = Boolean(0);
+// let e = Boolean(' ');
+// let result = a + b + c + d + e;
+// console.log(result)
+// 123456789falsetrue
